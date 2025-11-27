b1103bf3d69c63d89f1563abbbfe186bc49a6f86
ArmanSholihin <armanshlhn@gmail.com>
2025-11-28 01:00:35 +0700

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+function page() {
+  return (
+    <>
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis tenetur aut et est ab non deserunt ducimus doloribus, voluptates enim saepe voluptatibus voluptate quam dignissimos a, odit natus itaque mollitia. Officiis odit quo nesciunt vel laborum culpa alias eveniet possimus est, dicta quibusdam consequuntur amet non voluptas ab. Esse, molestiae voluptas. Modi ullam sapiente culpa quod accusantium minus voluptatem quo autem nulla aut esse ratione eius, officiis magni nobis vitae! Quidem assumenda tenetur quam reprehenderit, recusandae inventore velit aliquid mollitia a! Recusandae ad distinctio aperiam libero excepturi iure quos architecto facilis cupiditate, aspernatur maiores repellat sunt ratione ab consequuntur dolor velit. Expedita fugit unde fugiat repudiandae. Nesciunt eius deserunt, aliquid pariatur quidem ex praesentium illo aperiam magni numquam nemo. Harum ratione voluptas pariatur dolore ea recusandae odio impedit veritatis eaque architecto voluptatum, minus laudantium? Vitae, iste. Eius minima cum quisquam excepturi ratione nisi aliquid aspernatur repellendus sunt. Cumque sit corporis ipsum placeat mollitia quidem, accusamus ex illum, harum esse aliquid repudiandae nemo reprehenderit nobis atque magnam repellendus est enim fugit, nam omnis perspiciatis! Velit cumque, repellendus reprehenderit vero enim dolores iste incidunt nihil saepe at neque expedita quis, perferendis consectetur assumenda nam suscipit ea laborum asperiores debitis minima repudiandae labore.</p>
+    </>
+  )
+}
+
+export default page
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo">
+            <Link href="/">Logo</Link>
+        </div>
+
+              <div className="nav-links">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+      </div>
+      </nav>
+
+    </header>
+  )
+}
+
+export default Header
